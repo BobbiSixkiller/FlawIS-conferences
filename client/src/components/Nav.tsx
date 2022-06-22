ec772773ed89f18b499271240d96f60761031a5c
Matus Muransky <matus.muransky2@gmail.com>
2022-06-22 12:24:42 +0200

    Homepage in progress

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -5,6 +5,7 @@ import {
 	Dropdown,
 	Menu,
 	Icon,
+	Label,
 	Segment,
 	Sidebar,
 	Header,
@@ -18,40 +19,38 @@ import logoInverted from "public/images/Flaw-logo-notext-inverted.png";
 import logo from "public/images/Flaw-logo-notext.png";
 
 import useWidth from "src/hooks/useWidth";
-import { useRef } from "react";
 
 interface navProps {
 	inView: boolean;
 	width: number;
 }
 
-const FollowingBar = styled.div`
+const FollowingBar = styled.div<navProps>`
 	position: fixed;
+	z-index: 900;
 	top: 0px;
 	left: 0%;
 	padding: ${(props: navProps) =>
 		props.inView && props.width > 992 ? "2em 0em" : "0em 0em"};
-	background-color: ${(props: navProps) =>
-		props.inView ? "transparent" : "#FFFFFF"};
+	background-color: ${(props) => (props.inView ? "transparent" : "#FFFFFF")};
 	width: 100%;
-	box-shadow: ${(props: navProps) =>
+	box-shadow: ${(props) =>
 		props.inView
 			? "0px 0px 0px 0px transparent"
 			: "0px 3px 5px rgba(0, 0, 0, 0.2)"};
-	border-bottom: ${(props: navProps) =>
+	border-bottom: ${(props) =>
 		props.inView ? "1px solid transparent" : "1px solid #DDDDDD"};
 	transition: padding 0.5s ease, background 0.5s ease, box-shadow 0.5s ease,
 		border 0.5s ease;
 `;
 
 export default function Nav({ children }) {
-	const { ref, inView } = useInView({ treshold: 1 });
+	const { ref, inView } = useInView({ treshold: 1, initialInView: true });
 	const [opened, toggle] = useState(false);
-	useRef();
-
-	const width = useWidth();
 
 	console.log(inView);
+
+	const width = useWidth();
 
 	return (
 		<Sidebar.Pushable>
@@ -99,18 +98,7 @@ export default function Nav({ children }) {
 			</Sidebar>
 
 			<Sidebar.Pusher dimmed={opened}>
-				<Segment
-					inverted
-					vertical
-					style={{
-						padding: "0em",
-						marginBottom: "0px",
-						minHeight: "185px",
-						overflow: "hidden",
-						zIndex: 3,
-					}}
-				>
-					<div ref={ref} />
+				<div ref={ref}>
 					<FollowingBar inView={inView} width={width}>
 						<Container>
 							<Menu inverted={inView ? true : false} secondary size="large">
@@ -146,25 +134,40 @@ export default function Nav({ children }) {
 							</Menu>
 						</Container>
 					</FollowingBar>
+				</div>
+				<Segment
+					inverted
+					vertical
+					style={{
+						padding: "0em",
+						marginBottom: "0px",
+						minHeight: "185px",
+						overflow: "hidden",
+						zIndex: 3,
+						background:
+							"radial-gradient(circle, rgba(180,104,122,1) 34%, rgba(2,0,36,1) 100%)",
+						transition:
+							"background 6s cubic-bezier(0.680, -0.550, 0.265, 1.4) 2s, opacity 6s cubic-bezier(0.680, -0.550, 0.265, 1.4) 2s",
+					}}
+				>
 					<Container
 						text
 						textAlign="center"
 						style={{
-							marginTop: "50px",
 							minHeight: "350px",
 							height: "auto",
-							padding: "6rem 0rem",
+							margin: width > 800 ? 0 : "50px",
+							padding: width > 600 ? "15rem 0rem" : "6rem 0rem",
 						}}
 					>
+						<Label color="black">2.0.0</Label>
 						<Header
 							as="h1"
 							content="Conferences"
 							inverted
 							style={{
 								fontSize: width > 992 ? "4em" : "2em",
-								fontWeight: "normal",
-								//marginBottom: 0,
-								marginTop: width > 992 ? "3em" : "1.5em",
+								//fontWeight: "normal",
 							}}
 						/>
 						<Header
@@ -174,7 +177,6 @@ export default function Nav({ children }) {
 							style={{
 								fontSize: width > 992 ? "1.7em" : "1.5em",
 								fontWeight: "normal",
-								marginTop: width > 992 ? "1.5em" : "0.5em",
 							}}
 						/>
 						<Link href="/login">
