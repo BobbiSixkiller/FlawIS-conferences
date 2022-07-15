67c59d58d7b2a11600e0e05a90cd18f4be521eae
Matus Muransky <matus.muransky2@gmail.com>
2022-07-15 13:27:17 +0200

    Profile page in progress

--- client/src/pages/user/profile.tsx
+++ client/src/pages/user/profile.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
-import { useContext } from "react";
-import { Container, Grid, Menu, Segment, Tab } from "semantic-ui-react";
+import { FC, useContext, useState } from "react";
+import { Grid, Menu, Segment } from "semantic-ui-react";
 import Footer from "src/components/Footer";
 import Nav from "src/components/Nav";
 import { AuthContext } from "src/providers/Auth";
@@ -15,6 +15,7 @@ const AttendedConferences: FC = () => {
 
 const ProfilePage: NextPage = () => {
   const { user } = useContext(AuthContext);
+  const [tab, setTab] = useState("personal");
 
   return (
     <Grid style={{ marginTop: "60px" }} columns={2} container stackable>
@@ -22,11 +23,24 @@ const ProfilePage: NextPage = () => {
         <Grid.Column width={4}>
           <Menu fluid vertical>
             <Menu.Item header>{user.name}</Menu.Item>
-            <Menu.Item as="a" name="personal information" />
-            <Menu.Item as="a" name="conferences" />
+            <Menu.Item
+              as="a"
+              name="personal information"
+              active={tab === "personal"}
+              onClick={() => setTab("personal")}
+            />
+            <Menu.Item
+              as="a"
+              name="conferences"
+              active={tab === "conferences"}
+              onClick={() => setTab("conferences")}
+            />
           </Menu>
         </Grid.Column>
-        <Grid.Column width={12}></Grid.Column>
+        <Grid.Column width={12}>
+          {tab === "personal" && <PersonalInfo />}
+          {tab === "conferences" && <AttendedConferences />}
+        </Grid.Column>
       </Grid.Row>
     </Grid>
   );
